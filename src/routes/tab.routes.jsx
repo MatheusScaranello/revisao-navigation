739eee734023a26945ca207e58d2745e5ea9b1eb
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-03-11 15:54:21 -0300

    Add tab bar icons to bottom navigation

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import Category from "../screens/Category";
@@ -9,9 +10,33 @@ const Tab = createBottomTabNavigator();
 const TabRoutes = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Category" component={Category} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={"Black"} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={"Black"} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Category"
+        component={Category}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" color={"Black"} size={24} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
